106730bf8c16f16f02d14aee9f8497a7d1db9fac
DivOnGit <divyam.lakshya@gmail.com>
2025-09-22 10:14:40 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
             <TechStack />
             <Testimonials />
             <Contact />
-            <SpeedInsights />
+            {/*<SpeedInsights />*/}
         </>
     )
 }
